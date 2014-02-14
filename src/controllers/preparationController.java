8b87dd7a9133831808c45c6582399da0091f42c2
andreabeilei <andreabeilei@hotmail.com>
2014-02-14 16:07:21 +0100

    preparationController is updated

--- src/controllers/preparationController.java
+++ src/controllers/preparationController.java
@@ -3,7 +3,9 @@ package controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import model.DinnerModel;
+import model.Dish;
 
 
 /**
@@ -26,7 +28,35 @@ public class preparationController {
         // for(Dish d : dM.getFullMenu()) { code }
         //
         // the d then is one dish object which has all the properties
-        preparationMenuBox.getChildren().add(new Label(dM.toString()));
+
+
+        for(Dish d : dM.getFullMenu())
+        {
+
+            Label label = new Label();
+            Text text = new Text();
+            text.maxWidth(400);
+            text.setWrappingWidth(550);
+
+            if(d.getType() == 1)
+            {
+                label.setText("Starter");
+            }
+            else if(d.getType()==2)
+            {
+                label.setText("Main");
+            }
+            else if(d.getType()==3)
+            {
+                label.setText("Desert");
+            }
+
+            text.setText(d.getDescription());
+            preparationMenuBox.getChildren().add(label);
+            preparationMenuBox.getChildren().add(text);
+
+
+        }
     }
 
 
